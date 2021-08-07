7684a6839196fa88f03e1fb4c4f522f03709ab11
Abyss Studios <72343612+AbyssStudios@users.noreply.github.com>
2021-08-07 09:50:47 -0500

    1.2.0 Final Commit
    Bastion chests added
    Ruined portal chests added
    Modified piglin bartering gameplay: added fire charge
    Edited mod data to sync with datapack
    Added release to releases/1/2/0 folder
    Refactored release folder
    Added super secret verification information (shh)
    
    Co-Authored-By: jsaperr <72521489+jsaperr@users.noreply.github.com>

---
+++ mod/src/main/java/pw/abyssstudios/pogloot/Main.java
@@ -0,0 +1,10 @@
+package pw.abyssstudios.pogloot;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
